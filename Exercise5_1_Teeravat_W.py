487d354a649a7fde4f0bb18a3f4572aa0996b036
Teeravat Wongviseskij <112818691+teeravatw19@users.noreply.github.com>
2023-10-17 00:12:38 +0700

    Add files via upload

---
+++ Exercise5_1_Teeravat_W.py
@@ -0,0 +1,6 @@
+a = 10
+b = 2
+print(f"10 + 2 = {a+b}")
+print(f"10 - 2 = {a-b}")
+print(f"10 * 2 = {a*b}")
+print(f"10 / 2 = {a/b}")
---
+++ Exercise5_2_Teeravat_W.py
@@ -0,0 +1,3 @@
+s = float(input("ระยะทาง (km.): "))
+t = float(input("เวลา (s.): "))
+print(f"{s/t} km/h")
