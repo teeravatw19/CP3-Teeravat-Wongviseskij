6e88817b94d3ff70613a021325ee3756e952c78b
Teeravat Wongviseskij <112818691+teeravatw19@users.noreply.github.com>
2023-10-24 23:48:20 +0700

    Add files via upload

---
+++ Lecture50_Homework.py
@@ -0,0 +1,14 @@
+def plus(x,y):
+    print(x+y)
+def minus(x,y):
+    print(x-y)
+def multiply(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x/y)
+a = float(input("1st : "))
+b = float(input("2nd : "))
+plus(a,b)
+minus(a,b)
+multiply(a,b)
+divide(a,b)
